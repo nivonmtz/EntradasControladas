0d288352c883d3448535a6e05cb2a3269c23e221
nivonmtz <joannamartzz.3@gmail.com>
2024-04-26 14:33:27 -0700

    Creé una entrada controlada en React

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, {useState} from 'react'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+    // Crear un estado llamado miTexto
+    const[miTexto, setInputValue] = useState('');
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    // Crear función handleText
+    const handleInputChange =(event)=> {
+      const value = event.target.value; 
+      setInputValue(value);
+
+    };
+   
+
+    return (
+        <div>
+          <div>
+            Capture su texto
+          </div>
+          <input
+          type="text"
+          value={miTexto}
+          onChange={handleInputChange}
+          />
+          <h3>Su Texto</h3>
+          <p>
+            {miTexto}
+          </p>
+        </div>
+    );
 }
 
-export default App
+export default App;
